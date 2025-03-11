b622b8b029566407fc9cfe6aab144e3d853dc452
leroyymerlin <appleasedj@inbox.ru>
2025-03-11 07:54:04 +0300

    добавление файла для митапа RestClient

--- chikina/build.gradle.kts
+++ chikina/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("java")
+    id ("org.openapi.generator") version "7.7.0"
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "org.example"
@@ -10,6 +13,9 @@ repositories {
 }
 
 dependencies {
+    //implementation("org.springframework:spring-context:6.2.0")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    //implementation("org.springframework:spring-web:6.0.11")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
